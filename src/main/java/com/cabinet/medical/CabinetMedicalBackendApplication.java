0909647a415769732cddbe91e370bc7c62a60ebe
blcoussama <belcadioussama.eng@gmail.com>
2025-12-28 14:39:13 +0100

    Initial Spring Boot setup - PostgreSQL configured

---
+++ src/main/java/com/cabinet/medical/CabinetMedicalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cabinet.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetMedicalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinetMedicalBackendApplication.class, args);
+	}
+
+}
